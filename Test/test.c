19725ad788355eb2e3d4d306e7ac090ac03e28ee
Jiajun Zou <1589228259@qq.com>
2021-02-11 15:32:16 -0800

    start A4

--- Test/test.c
+++ Test/test.c
@@ -2,10 +2,5 @@
 #include<stdlib.h>
 
 int main() {
-    int a[] = { 8, 9, 10, 11, 12 };
-    int* i = &a[0];
-    printf("%d\n", &i[0]);
-    printf("%d\n", i);
-    printf("%d\n", *i);
-    printf("%d\n", *(i+4));
+    printf("hello world");
 }
