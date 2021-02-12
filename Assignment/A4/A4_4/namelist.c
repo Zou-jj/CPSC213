41d29fbe446e2345a9f41e8d9977399ef375ec9a
Jiajun Zou <1589228259@qq.com>
2021-02-12 14:27:22 -0800

    start A4_8

--- Assignment/A4/A4_4/namelist.c
+++ Assignment/A4/A4_4/namelist.c
@@ -7,7 +7,7 @@ int num_names = 0;
 
 void add_name(char *name) {
   // Allocate space for a copy of the name
-  names[num_names] = malloc(sizeof(name));
+  names[num_names] = malloc(strlen(name) + 1);
   // Copy name to new area in memory
   strcpy(names[num_names], name);
   // Add one to num_names
