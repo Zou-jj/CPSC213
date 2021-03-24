3d873740f0468e8e8c37849c3f49375129c4f789
Jiajun Zou <1589228259@qq.com>
2021-03-24 16:08:02 -0700

    added A8_5

---
+++ Assignment/A8/A8_5/test.s
@@ -0,0 +1,17 @@
+    ld  $0, r0
+    ld  $100, r1
+    ld  $200, r2
+    ld  $300, r3
+    ld  $400, r4
+    ld  $500, r5
+    ld  $600, r6
+    ld  $700, r7
+    mov r1, r0
+    add r1, r2
+    and r2, r3
+    inc r4
+    inca r5
+    dec r6
+    deca r7
+    not r7
+    halt
